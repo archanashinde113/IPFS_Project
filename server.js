af0193e2561959adf6d16b5678e042a1778c3ab2
Archana_Shinde <archanashinde113@gmail.com>
2022-04-13 12:48:20 +0530

    ipfs_project

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-// const express = require('express')
-// const mongoose = require('mongoose')
-// const dotenv = require('dotenv')
-// dotenv.config();
-
-// const app = express();
-// // // app.use(express.static(__dirname + '/public'));
-// require("./routes/ipfs.route")(app);
-
-
-//   mongoose.connect(process.env.mongoURI,()=> {
-//       console.log('mongoose connected')
-
-//   })
- 
-
-// app.listen(8080,( ) => {
-//     console.log('server running at port 8080')
-// });
-
-
-
 
 const express = require('express')
 const mongoose = require('mongoose')
@@ -28,20 +6,11 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 const app = express();
-// var ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
-// 
 
-const x = require('./routes/ipfs.route.js')
+
+const ipfs = require('./routes/ipfs.route.js')
 app.use('/ipfs',ipfs);
-// app.use(express.static(path.resolve(process.cwd(), 'public/index.html')))
 
-
-
-
-    
-
-
-  
 
   mongoose.connect(process.env.mongoURI,()=> {
       console.log('mongoose connected')
